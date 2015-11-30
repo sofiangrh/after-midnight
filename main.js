bb5c5c14b003920acdf8ef7a67ad29ea3ea44400
sofianugraha <137006200@student.unsil.ac.id>
2015-11-30 09:21:30 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+app.on('ready', function() {
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+
+  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+
+  mainWindow.webContents.openDevTools();
+
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+});
