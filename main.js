e3afb430822cfe998a946927000e69281e5def21
sofianugraha <137006200@student.unsil.ac.id>
2015-12-11 05:41:34 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 app.on('ready', function() {
   mainWindow = new BrowserWindow({width: 800, height: 600});
 
-  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+  mainWindow.loadUrl('file://' + __dirname + '/index.html'); //none yet
 
   mainWindow.webContents.openDevTools();
 
